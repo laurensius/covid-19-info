9f147e5671dc59d1d8d7e13768650e633c9235b4
laurensius <burglar.tea@gmail.com>
2020-03-17 15:11:45 +0700

    add table

--- application/views/apps/index.php
+++ application/views/apps/index.php
@@ -3,6 +3,14 @@
     <link rel="stylesheet" 
     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+    <style>
+      .map {
+        height: 400px;
+        width: 100%;
+      }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
@@ -11,18 +19,27 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Kasus Global <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
-        </div>
+        </div> -->
     </nav>
+
     <div class="container">
         <div class="row" style="margin-top:15px">
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <div class="card" style="width: 18rem;">
+            <div class="col-lg-12">
+                <!-- <div id="map" class="map"></div> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row" style="margin-top:15px">
+            <div class="col">
+                <div class="card" >
                     <div class="card-body">
                         <h5 class="card-title">
                             <b>
@@ -35,8 +52,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <div class="card" style="width: 18rem;">
+            <div class="col">
+                <div class="card" >
                     <div class="card-body">
                         <h5 class="card-title">
                             <b>
@@ -49,8 +66,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <div class="card" style="width: 18rem;">
+            <div class="col">
+                <div class="card" >
                     <div class="card-body">
                         <h5 class="card-title">
                             <b>
@@ -65,10 +82,64 @@
             </div>  
         </div>
     </div>
+
+    <div class="container">
+        <div class="row" style="margin-top:15px">
+            <div class="col-lg-12">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Negara / Regional</th>
+                            <th>Terkonfirmasi</th>
+                            <th>Meninggal</th>
+                            <th>Sembuh</th>
+                            <th>Aktif</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        for($x=0;$x<sizeof($data["detail_kasus"]);$x++){
+                        ?>
+                        <tr>
+                            <td><?php echo ($x+1) ?></td>
+                            <td><?php echo $data["detail_kasus"][$x]["regional_negara"] ?></td>
+                            <td><?php echo $data["detail_kasus"][$x]["terkonfirmasi"] ?></td>
+                            <td><?php echo $data["detail_kasus"][$x]["meninggal"] ?></td>
+                            <td><?php echo $data["detail_kasus"][$x]["sembuh"] ?></td>
+                            <td><?php echo $data["detail_kasus"][$x]["aktif"] ?></td>
+                        </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
     integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+    <script type="text/javascript">
+    
+    var global_data = <?php echo json_encode($data["detail_kasus"]); ?>;
+
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+            source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([120, 0]),
+            zoom: 3
+        })
+    });
+    </script>
 </body>
